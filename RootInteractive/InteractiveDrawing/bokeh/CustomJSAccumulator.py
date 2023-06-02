f1bd0c64666460a4263b6052eb7e59412d81b182
Marian Ivanov <majo.ivanov@gmail.com>
2023-06-02 08:44:22 +0200

    Initial custom reducer commit

---
+++ RootInteractive/InteractiveDrawing/bokeh/CustomJSAccumulator.py
@@ -0,0 +1,12 @@
+from bokeh.model import Model
+from bokeh.core.properties import String, Dict, List, Any
+
+class CustomJSAccumulator(Model):
+    __implementation__ = "CustomJSAccumulator.ts"
+
+    parameters = Dict(String, Any, help="Extra arguments to call the function with")
+    fields = List(String, default=["x"], help="List of positional arguments - might be made optional in the future")
+    initial_value = Any(help="Initial value for the accumulator - ignored if v_func is specified")
+    func = String(serialized=True, help="Code to be computed on the client - scalar case")
+    v_func = String(serialized=True, help="Code to be computed on the client - vector case")
+
